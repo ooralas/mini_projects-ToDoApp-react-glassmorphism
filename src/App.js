60cd588a0dac8708d6786c2d52a751981c7228fa
Salar Al Ali <ooralas@outlook.com>
2022-03-20 17:35:10 +0100

    project with build version

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import List from './Component/list/list'
+
+function App() {
+  let task = [
+    { title: 'React-Aufgabe bearbeiten ..', done: false, id: 1 },
+    { title: 'Tanken gehen', done: false, id: 2 },
+    { title: 'React Kurs weiter machen', done: false, id: 3 },
+  ]
+  return (
+    <div className='App'>
+      <List className='list' tasks={task} />
+    </div>
+  )
+}
+
+export default App
